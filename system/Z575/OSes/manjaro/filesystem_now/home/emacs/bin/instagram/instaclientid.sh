e78357d984e0e1fa0344a4d0d289bec40fe1f8e4
Emanuel Czirai <zazdxscf@gmail.com>
2015-08-19 08:13:28 +0200

    add instagram script for metadata extraction
    things that devs have access to, which may not seem obvious to normal
    users
    (like exact latitude/longitude info for the pics that include location
    information)

---
+++ system/Z575/OSes/manjaro/filesystem_now/home/emacs/bin/instagram/instaclientid.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#this id is actually public informaiton (google-able, that's how I found it), so I don't mind including it
+export cid="`echo YWIxMDNlNTRjNTQ3NDdhZGE5ZTgwNzEzN2RiNTJkNzcK | openssl base64 -d`"
+
