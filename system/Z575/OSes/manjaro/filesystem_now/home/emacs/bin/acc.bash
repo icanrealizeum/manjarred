20f33330a5317a4b4966b015bb03a0da8ec5e238
Emanuel Czirai <zazdxscf@gmail.com>
2015-09-26 16:15:20 +0200

    add my safe4 scripts for now
    I'll see whether to get rid of them later

---
+++ system/Z575/OSes/manjaro/filesystem_now/home/emacs/bin/acc.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+__die() { local ec=$1;shift;echo "$@" >&2 ; exit $ec; }
+source funx.bash || __die 100 "funx.bash not in PATH"         
+
+fil="safe4"
+usr="emacs"
+
+tcmountcontainer.bash "/home/emacs/E/pri vate/${fil}" "/${fil}" "$usr"
+
