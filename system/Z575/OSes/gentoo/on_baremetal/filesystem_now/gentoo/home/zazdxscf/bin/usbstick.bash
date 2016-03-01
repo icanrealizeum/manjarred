34aaeac56eb844339b964d0cb155d10f23c61185
Emanuel Czirai <aoaaxy+github@gmail.com>
2016-03-01 16:20:48 +0100

    sys updates & libreoffice fix
    * fix /etc/portage/bashrc for eapi 6
    * app-office/libreoffice-9999 attempt bash completion patch workaround - still compiling
    * usbstick will launch a new terminal (tho can't wait for it to be exited)
    * backup next kernel config linux-4.5-rc6
    * this introduced python-exec after update

--- system/Z575/OSes/gentoo/on_baremetal/filesystem_now/gentoo/home/zazdxscf/bin/usbstick.bash
+++ system/Z575/OSes/gentoo/on_baremetal/filesystem_now/gentoo/home/zazdxscf/bin/usbstick.bash
@@ -10,6 +10,7 @@ dev='/dev/sdc1'
 #this makes all files u+rw,a-rwx  where u is zazdxscf(or $usr)
 sudo mount "$dev" "$fil" -o,uid=${usr},noexec,nosuid,nodev,fmask=0177
 if test "$?" -eq '0'; then
+  xfce4-terminal
   safepromptkey "press a key when done, to unmount"
   sudo umount "$fil"
   sync
