c2de49fde33634e777544a4a65ee95ba2f4d9f34
ActualizeInMaterial <ActualizeInMaterial@gmail.com>
2016-06-14 22:15:46 +0300

    LARK downloads' checksums

---
+++ system/LARK/APKs/external/dictionaries/hedgedict/getall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#1. For using sounds in HedgeDict you need to download file ans copy it to snd folder in your HedgeDict system folder (by default is root of your SD-card)
+#Also you can use your own sonds files. For add own files simple copy them to the snd/{language}/ in your HedgeDict system folder. You need to use *.ogg file format for sounds.
+#2. These databases you can use on your Android device. For install download base on your device and paste it to the HedgeDict system directory (by defalut is located in root of your SD-card).
+
+
+wget http://hedgedict.com/data/app/HedgeDict.apk
+wget http://hedgedict.com/data/snd/sound-en.db
+wget http://hedgedict.com/data/download/English-Hungarian_dictionary.db
+wget http://hedgedict.com/data/download/Hungarian-English_dictionary.db
+
+wget http://hedgedict.com/data/snd/sound-de.db
+wget http://hedgedict.com/data/snd/sound-fr.db
+
+
+wget http://hedgedict.com/data/download/EN-EN-ConciseOxford%20EnglishDictionary.db
+wget http://hedgedict.com/data/download/EN-EN-IdiomsDictionary.db
+wget http://hedgedict.com/data/download/EN-EN-OxfordAdvancedLearnersDictionary.db
+wget http://hedgedict.com/data/download/EN-EN-Thesaurus.db
+
+wget http://hedgedict.com/data/download/EN-FR.db
+wget http://hedgedict.com/data/download/English-Italian_dictionary.db
+wget http://hedgedict.com/data/download/English-Romanian_dictionary.db
+
+wget http://hedgedict.com/data/download/EN-DE-ConciseOxford.db
+wget http://hedgedict.com/data/download/EN-DE-Grosswoerterbuch.db
+
+wget http://hedgedict.com/data/download/DE-EN-ConciseOxford.db
+
+
