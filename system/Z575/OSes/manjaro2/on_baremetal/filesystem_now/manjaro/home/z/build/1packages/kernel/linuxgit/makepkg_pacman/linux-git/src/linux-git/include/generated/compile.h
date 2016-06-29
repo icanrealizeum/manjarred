5336c01d290a44c67ea42820ebb49d8718089db9
ActualizeInMaterial <ActualizeInMaterial@gmail.com>
2016-06-29 17:54:26 +0300

    fixed COMMIT to work
    * also fixed ssdwritten's refresh script

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 10 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#9 SMP PREEMPT Wed Jun 29 14:46:28 EEST 2016"
+#define UTS_VERSION "#10 SMP PREEMPT Wed Jun 29 17:27:29 EEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160602 (GCC) "
