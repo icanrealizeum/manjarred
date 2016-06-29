a905deb65830fbbaaf64a6ea16afc3511b83b7f9
ActualizeInMaterial <ActualizeInMaterial@gmail.com>
2016-06-29 15:21:18 +0300

    kernel recompilation 2 add missing iptables tables

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#8 SMP PREEMPT Tue Jun 28 04:10:17 EEST 2016"
+#define UTS_VERSION "#9 SMP PREEMPT Wed Jun 29 14:46:28 EEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160602 (GCC) "
