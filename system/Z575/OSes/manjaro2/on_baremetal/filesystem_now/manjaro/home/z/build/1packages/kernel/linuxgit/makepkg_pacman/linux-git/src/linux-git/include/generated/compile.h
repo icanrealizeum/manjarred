c5a72765666705dd3713655780a0eb3c3ccab154
icanrealizeum <icanrealizeum@gmail.com>
2016-08-07 01:36:05 +0300

    double the speed of freeing pagecache pages
    by doing them manually before "Unmounting /oldroot." does
    
    * update kernel
    * speedup systemd-udevd eg. udev.children-max=256 on kernel cmdline

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#8 SMP PREEMPT Sat Aug 6 19:09:43 EEST 2016"
+#define UTS_VERSION "#11 SMP PREEMPT Sun Aug 7 01:11:58 EEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160707 (GCC) "
--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/usr/lib/systemd/system-shutdown/debug.sh
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/usr/lib/systemd/system-shutdown/debug.sh
@@ -16,8 +16,9 @@ echo d > /proc/sysrq-trigger
 #'d' - Shows all locks that are held.
 #src: /home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/Documentation/sysrq.txt
 
-echo l > /proc/sysrq-trigger
+#echo l > /proc/sysrq-trigger
 echo h > /proc/sysrq-trigger
+#[ 4527.376142] sysrq: SysRq : HELP : loglevel(0-9) reboot(b) crash(c) terminate-all-tasks(e) memory-full-oom-kill(f) kill-all-tasks(i) thaw-filesystems(j) sak(k) show-backtrace-all-active-cpus(l) show-memory-usage(m) nice-all-RT-tasks(n) poweroff(o) show-registers(p)  show-all-timers(q) unraw(r) sync(s) show-task-states(t) unmount(u) force-fb(V) show-blocked-tasks(w) 
 echo m > /proc/sysrq-trigger
 #echo t > /proc/sysrq-trigger
 echo w > /proc/sysrq-trigger
@@ -35,12 +36,33 @@ echo -n 'After:';  cat /sys/block/sda/device/queue_depth
 hdparm -W /dev/sda
 hdparm -C /dev/sda
 
+#don't have tee or sysctl, have sudo(apparently)
+#workaround for: https://bugzilla.kernel.org/show_bug.cgi?id=151621#c2
+echo 'sync first'
+time sync
+echo "Before:"
+echo m > /proc/sysrq-trigger #has no effect: ; dmesg|grep -F "pagecache pages"|tail -1
+echo 'manually Freeing pagecache pages'
+#don't have sysctl: time sysctl vm.drop_caches=1
+#XXX: comment out following drop_caches line(s) to test deactivate_super's speed(it lingers at "Unmounting /oldroot")
+#To free pagecache(works fine):
+#time echo 1 >/proc/sys/vm/drop_caches
+#To free dentries and inodes(untested):
+#time echo 2 >/proc/sys/vm/drop_caches
+#To free pagecache, dentries and inodes:
+time echo 3 >/proc/sys/vm/drop_caches
+echo "After :"
+echo m > /proc/sysrq-trigger #has no effect: ;dmesg|grep -F "pagecache pages"|tail -1
+
+echo 'end of log'
+
 dmesg > /shutdown-log.txt
-sync && sdparm --command=sync /dev/sda && sleep 1
+time sync && sdparm --command=sync /dev/sda && sleep 1
 mount -o remount,ro /
 
 #src: https://unix.stackexchange.com/questions/55281/how-to-stop-waking-all-attached-drives-on-reboot-deactivating-swap/55417#55417
 #turn off drive cache, workaround for https://bugzilla.kernel.org/show_bug.cgi?id=151631
+#XXX: comment out following line to prevent rebooting from happening(due to aforementioned bug just 1 line above) after deactivate_super finished(aka when "Unmounting /oldroot" is done) to see the printk's time(uptime) so you can calc. the difference manually
 hdparm -W0 /dev/sda
 hdparm -W /dev/sda
 
