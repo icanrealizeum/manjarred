4473e1eba2de31d354bad746254ef51fa5221fea
icanrealizeum <icanrealizeum@gmail.com>
2016-08-09 01:44:39 +0300

    firefox-hg still coredump

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 20 */
+/* This file is auto generated, version 21 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#20 SMP PREEMPT Mon Aug 8 21:18:27 EEST 2016"
+#define UTS_VERSION "#21 SMP PREEMPT Tue Aug 9 01:42:02 EEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160707 (GCC) "
