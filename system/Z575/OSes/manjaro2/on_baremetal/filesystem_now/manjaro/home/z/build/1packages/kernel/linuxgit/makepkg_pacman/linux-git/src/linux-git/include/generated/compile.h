a11f4dd142db8bc450c70a6baa6de1cee9e34295
icanrealizeum <icanrealizeum@gmail.com>
2016-08-12 15:52:34 +0300

    recompiled firefox w/o webrtc
    real  68m19.879s
    user  149m33.047s
    sys 18m21.346s
    
    and other stuff

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 22 */
+/* This file is auto generated, version 27 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#22 SMP PREEMPT Wed Aug 10 22:39:26 EEST 2016"
+#define UTS_VERSION "#27 SMP PREEMPT Fri Aug 12 13:32:40 EEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160707 (GCC) "
