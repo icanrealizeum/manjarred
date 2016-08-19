2a006f158e1093bfa30fe4b29c1897852da51c4e
icanrealizeum <icanrealizeum@gmail.com>
2016-08-19 22:18:41 +0300

    fixed chromium
    * failed firefox recompilation - context menu still borked until this
    recompile succeeds!
    * kernel recompile w/o sync-ing SSD cache

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 43 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#43 SMP PREEMPT Mon Aug 15 21:19:40 EEST 2016"
+#define UTS_VERSION "#43 SMP PREEMPT Fri Aug 19 22:13:34 EEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
-#define LINUX_COMPILER "gcc version 6.1.1 20160707 (GCC) "
+#define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
