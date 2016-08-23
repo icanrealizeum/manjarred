95af11f9c97a09718f8a54d2c2f03946ca0f858c
icanrealizeum <icanrealizeum@gmail.com>
2016-08-23 22:41:46 +0300

    saving original lockPref patch
    which cannot be cleanly applied

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/firefox-hg/vendor.js
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/firefox-hg/vendor.js
@@ -5,6 +5,8 @@
 pref("intl.locale.matchOS", true);
 //works: pref("browser.startup.homepage",	"about:blank");
 //XXX: lockPref has no effect here!!! use pref() instead
+//unless patch applied!
+lockPref("security.turn_off_all_security_so_that_viruses_can_take_over_this_computer", false);
 
 // Disable default browser checking.
 pref("browser.shell.checkDefaultBrowser", false);
