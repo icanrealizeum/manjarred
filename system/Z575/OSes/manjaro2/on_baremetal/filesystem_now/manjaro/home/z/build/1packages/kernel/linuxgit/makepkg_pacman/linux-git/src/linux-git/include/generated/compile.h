bbdd8aea0263ac611eedc668a0a0dc591a0575ca
icanrealizeum <icanrealizeum@gmail.com>
2016-08-28 14:36:10 +0200

    add 2 extra sys-kernels for good measure

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 52 */
+/* This file is auto generated, version 53 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#52 SMP PREEMPT Sun Aug 28 14:03:32 CEST 2016"
+#define UTS_VERSION "#53 SMP PREEMPT Sun Aug 28 14:19:23 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
