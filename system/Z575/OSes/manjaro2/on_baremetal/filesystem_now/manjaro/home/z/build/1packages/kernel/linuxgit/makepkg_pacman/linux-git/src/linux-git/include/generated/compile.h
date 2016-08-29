0d02f8f3756205d829f083249e336ecd2f60dec8
icanrealizeum <icanrealizeum@gmail.com>
2016-08-29 18:31:04 +0200

    turn off acpi debug
    because it was on w/o pr_debug being off in cmdline
    * also update config.x86_64
    * unset CONFIG_ACPI_PCI_SLOT
    * fix rust patch for latest git
    * update pacman-git's makepkg.conf

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 68 */
+/* This file is auto generated, version 69 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#68 SMP PREEMPT Mon Aug 29 16:46:37 CEST 2016"
+#define UTS_VERSION "#69 SMP PREEMPT Mon Aug 29 17:52:44 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
