9d243e9aa0de2a50c172040d2da637b3a04b7ba8
icanrealizeum <icanrealizeum@gmail.com>
2016-08-29 18:51:07 +0200

    unset CONFIG_WQ_POWER_EFFICIENT_DEFAULT
    4.5sec boot time(to login prompt)

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 69 */
+/* This file is auto generated, version 70 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#69 SMP PREEMPT Mon Aug 29 17:52:44 CEST 2016"
+#define UTS_VERSION "#70 SMP PREEMPT Mon Aug 29 18:44:59 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
