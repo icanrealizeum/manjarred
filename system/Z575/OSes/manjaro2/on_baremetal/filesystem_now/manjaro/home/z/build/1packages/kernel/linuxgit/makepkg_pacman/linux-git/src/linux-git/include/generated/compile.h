cb4ae1e14220728a2ae09f96d074737929a5d999
icanrealizeum <icanrealizeum@gmail.com>
2016-08-29 20:21:20 +0200

    unset CONFIG_X86_SYSFB

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 71 */
+/* This file is auto generated, version 72 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#71 SMP PREEMPT Mon Aug 29 19:02:51 CEST 2016"
+#define UTS_VERSION "#72 SMP PREEMPT Mon Aug 29 19:42:53 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
