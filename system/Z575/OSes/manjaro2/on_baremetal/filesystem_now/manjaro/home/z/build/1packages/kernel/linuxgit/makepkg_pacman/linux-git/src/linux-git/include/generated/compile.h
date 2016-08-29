4f5be9cfa9a2e6d7ffcf37d6cffc2c43d7fbee15
icanrealizeum <icanrealizeum@gmail.com>
2016-08-29 08:28:25 +0200

    holy fuckies: just shaved 5 sec from boot time
    i think it was the stack protector! now everything is faster!!
    5.6sec boot time!

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 62 */
+/* This file is auto generated, version 64 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#62 SMP PREEMPT Mon Aug 29 04:19:19 CEST 2016"
+#define UTS_VERSION "#64 SMP PREEMPT Mon Aug 29 08:16:45 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
