a4dbde33687aabe61ade4022709802e2171307a1
icanrealizeum <icanrealizeum@gmail.com>
2016-08-29 19:07:09 +0200

    unset useless CONFIG_BACKLIGHT_LM3639

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 70 */
+/* This file is auto generated, version 71 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#70 SMP PREEMPT Mon Aug 29 18:44:59 CEST 2016"
+#define UTS_VERSION "#71 SMP PREEMPT Mon Aug 29 19:02:51 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
