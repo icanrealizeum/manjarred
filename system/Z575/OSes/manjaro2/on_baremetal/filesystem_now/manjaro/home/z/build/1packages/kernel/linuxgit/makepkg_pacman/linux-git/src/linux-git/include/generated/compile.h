2733513b0fba182dc3872a9452940e0cd3882ece
icanrealizeum <icanrealizeum@gmail.com>
2016-08-30 07:38:25 +0200

    modify global CFLAGS in makepkg.conf
    * rust compiled with less expensive flags
    * upd hosts
    * upd vlc and -f fix
    * firefox fails compilation
    * upd kernel

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 72 */
+/* This file is auto generated, version 74 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#72 SMP PREEMPT Mon Aug 29 19:42:53 CEST 2016"
+#define UTS_VERSION "#74 SMP PREEMPT Tue Aug 30 06:43:33 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/utsrelease.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.8.0-rc4-g3eab887"
+#define UTS_RELEASE "4.8.0-rc4-ge4e98c4"
