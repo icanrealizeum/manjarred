2bb4b44a0544dc0a8f49b70b4af290d65fb85a07
icanrealizeum <icanrealizeum@gmail.com>
2016-08-30 14:08:35 +0200

    microcode patch and msr
    * temp allow tcp and keyserver
    * remove ignore_loglevel from cmdline
    * kernel tested mail.patch to be good
      * forgot to reset CONFIG_VGACON_SOFT_SCROLLBACK_SIZE to 4096 after not using patches
    * kernel compile msr module but move it away for amdmsrtweaker-lnx
    * too hard to make bash's time report time even when C-c -ed

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 74 */
+/* This file is auto generated, version 76 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#74 SMP PREEMPT Tue Aug 30 06:43:33 CEST 2016"
+#define UTS_VERSION "#76 SMP PREEMPT Tue Aug 30 13:54:09 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
