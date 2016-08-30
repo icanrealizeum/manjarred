efe61c5f34bd3e598a53fa3a8c3103f0f23661ef
icanrealizeum <icanrealizeum@gmail.com>
2016-08-30 19:47:23 +0200

    lil chromium update

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 76 */
+/* This file is auto generated, version 77 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#76 SMP PREEMPT Tue Aug 30 13:54:09 CEST 2016"
+#define UTS_VERSION "#77 SMP PREEMPT Tue Aug 30 17:55:55 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
