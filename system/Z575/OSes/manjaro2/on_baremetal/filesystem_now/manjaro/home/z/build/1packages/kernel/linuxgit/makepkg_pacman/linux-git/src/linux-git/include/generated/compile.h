5f5f44eb8ec8f0d9ae1f377c32780cc2ac29b2ef
icanrealizeum <icanrealizeum@gmail.com>
2016-08-31 18:23:49 +0200

    a lot of stuff/rsync/updates
    don't even remember
    also testing -Os flag

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 77 */
+/* This file is auto generated, version 79 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#77 SMP PREEMPT Tue Aug 30 17:55:55 CEST 2016"
+#define UTS_VERSION "#79 SMP PREEMPT Wed Aug 31 18:07:58 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
