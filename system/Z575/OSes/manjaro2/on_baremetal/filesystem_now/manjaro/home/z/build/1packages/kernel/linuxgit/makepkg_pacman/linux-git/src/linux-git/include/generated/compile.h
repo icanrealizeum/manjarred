a58f40b4e35d2758809317eea1804cf4ac6aa653
icanrealizeum <icanrealizeum@gmail.com>
2016-09-01 06:48:13 +0200

    firefox still fail with same error
    kernel/chromium still (re)compiling

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 81 */
+/* This file is auto generated, version 83 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#81 SMP PREEMPT Wed Aug 31 19:20:45 CEST 2016"
+#define UTS_VERSION "#83 SMP PREEMPT Thu Sep 1 06:12:07 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
