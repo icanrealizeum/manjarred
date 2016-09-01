8beda457ce8c864482a5291b141c7dc276192ef7
icanrealizeum <icanrealizeum@gmail.com>
2016-09-01 02:40:14 +0200

    compiling chromium/firefox/kernel -Ofast
    * hopefully firefox is fixed by this aom=>dom moz.config
    * acpica(aka acpidump) update
    * chromium all.patch update to match upstream changes
    * get back original filename for erratum 665

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 79 */
+/* This file is auto generated, version 81 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#79 SMP PREEMPT Wed Aug 31 18:07:58 CEST 2016"
+#define UTS_VERSION "#81 SMP PREEMPT Wed Aug 31 19:20:45 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/utsrelease.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.8.0-rc4-g86a1679+"
+#define UTS_RELEASE "4.8.0-rc4-g071e31e"
