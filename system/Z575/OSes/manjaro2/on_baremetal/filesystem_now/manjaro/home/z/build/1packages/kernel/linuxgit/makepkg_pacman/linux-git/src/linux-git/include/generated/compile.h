f8243cc583a6fed489b024efe0630fd9c5625cbb
icanrealizeum <icanrealizeum@gmail.com>
2016-09-01 09:03:30 +0200

    move gst packages so i know their order

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 85 */
+/* This file is auto generated, version 86 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#85 SMP PREEMPT Thu Sep 1 07:47:36 CEST 2016"
+#define UTS_VERSION "#86 SMP PREEMPT Thu Sep 1 08:18:08 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
