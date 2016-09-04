96f59d1410e689d4baefed5892a9ee5b39fa69dc
icanrealizeum <icanrealizeum@gmail.com>
2016-09-04 20:27:26 +0200

    update kernel
    665

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 91 */
+/* This file is auto generated, version 92 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#91 SMP PREEMPT Sun Sep 4 08:20:15 CEST 2016"
+#define UTS_VERSION "#92 SMP PREEMPT Sun Sep 4 19:25:38 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/utsrelease.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.8.0-rc4-g28e6815"
+#define UTS_RELEASE "4.8.0-rc4-g9ca581b"
