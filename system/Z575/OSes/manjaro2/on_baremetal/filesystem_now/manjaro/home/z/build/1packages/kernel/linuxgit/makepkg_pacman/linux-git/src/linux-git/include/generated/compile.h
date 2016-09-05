b229b7592112997e4a4c9d9df8f3ec87204a5621
icanrealizeum <icanrealizeum@gmail.com>
2016-09-05 13:28:49 +0200

    still works w/o prepending, but later
    [    1.739976] microcode: CPU0: patch_level=0x03000014
    
    [    3.771109] microcode: CPU0: new patch_level=0x03000027

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 94 */
+/* This file is auto generated, version 95 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#94 SMP PREEMPT Mon Sep 5 13:13:48 CEST 2016"
+#define UTS_VERSION "#95 SMP PREEMPT Mon Sep 5 13:25:29 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
