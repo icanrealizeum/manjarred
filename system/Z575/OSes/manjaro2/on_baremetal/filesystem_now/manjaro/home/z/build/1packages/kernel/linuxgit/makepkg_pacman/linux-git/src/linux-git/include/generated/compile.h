15fb711fec07a3b8c7141ddc8c468a2a441003ba
icanrealizeum <icanrealizeum@gmail.com>
2016-09-06 13:33:05 +0200

    sysup and other stuff
    trying ffmpeg with -O2 and no mtune

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 95 */
+/* This file is auto generated, version 98 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#95 SMP PREEMPT Mon Sep 5 13:25:29 CEST 2016"
+#define UTS_VERSION "#98 SMP PREEMPT Tue Sep 6 07:32:23 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/utsrelease.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.8.0-rc5-gc693593"
+#define UTS_RELEASE "4.8.0-rc5-gbc4dee5"
