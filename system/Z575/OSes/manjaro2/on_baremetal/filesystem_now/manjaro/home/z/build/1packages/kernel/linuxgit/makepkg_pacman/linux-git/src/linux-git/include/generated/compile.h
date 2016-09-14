8dc600fd183d16150a52d4164878536ba691da61
icanrealizeum <icanrealizeum@gmail.com>
2016-09-14 23:27:23 +0200

    recompile pacman
    + chromium changes are in progress

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 100 */
+/* This file is auto generated, version 101 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#100 SMP PREEMPT Wed Sep 14 09:15:15 CEST 2016"
+#define UTS_VERSION "#101 SMP PREEMPT Wed Sep 14 23:12:56 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.1.1 20160802 (GCC) "
