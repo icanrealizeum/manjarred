2ff820a30ad3aa909119fb1aae72a78bbd5f8cef
icanrealizeum <icanrealizeum@gmail.com>
2016-09-18 06:28:35 +0200

    sysupdate and stuff

--- system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
+++ system/Z575/OSes/manjaro2/on_baremetal/filesystem_now/manjaro/home/z/build/1packages/kernel/linuxgit/makepkg_pacman/linux-git/src/linux-git/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 103 */
+/* This file is auto generated, version 104 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#103 SMP PREEMPT Sat Sep 17 15:40:32 CEST 2016"
+#define UTS_VERSION "#104 SMP PREEMPT Sat Sep 17 22:54:40 CEST 2016"
 #define LINUX_COMPILE_BY "z"
 #define LINUX_COMPILE_HOST "myzee"
 #define LINUX_COMPILER "gcc version 6.2.1 20160830 (GCC) "
